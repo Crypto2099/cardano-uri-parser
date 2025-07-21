a206609b0e45f7fd4c7f30aa93643d9f13e5e2ee
Adam Dean <adam@crypto2099.io>
2025-07-20 17:08:37 -0700

    Fix a few issues with CI/CD pipeline actions

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,25 @@
-/** @type {import('jest').Config} */
+/** @type {import("jest").Config} */
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    testMatch: ['**/tests/**/*.test.ts'],
-    moduleFileExtensions: ['ts', 'js', 'json'],
+    preset: "ts-jest",
+    testEnvironment: "node",
+    testMatch: ["**/tests/**/*.test.ts"],
+    moduleFileExtensions: [
+        "ts",
+        "js",
+        "json"
+    ],
     collectCoverage: true,
-    coverageDirectory: 'coverage',
-    coverageReporters: ['text', 'lcov'],
+    coverageDirectory: "coverage",
+    coverageReporters: [
+        "text",
+        "lcov"
+    ],
+    coverageThreshold: {
+        "global": {
+            "branches": 90,
+            "functions": 90,
+            "lines": 90,
+            "statements": 90
+        }
+    }
 };
